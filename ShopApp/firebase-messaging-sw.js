16dbef8d3fd0d4ab7e72a8d09e4a038a7c5dae38
Vigneshwar-N <vigneshwar2k2@gmail.com>
2024-09-18 10:56:35 +0530

    modified shop

---
+++ ShopApp/firebase-messaging-sw.js
@@ -0,0 +1,8 @@
+import {AppRegistry} from 'react-native';
+import messaging from '@react-native-firebase/messaging';
+
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
+});
+
+AppRegistry.registerComponent('appName', () => App);
