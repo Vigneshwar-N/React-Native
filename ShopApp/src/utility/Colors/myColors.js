e7c1e6b2771914382e4c397a12fb0ac4285ee84d
Vigneshwar-N <vigneshwar2k2@gmail.com>
2024-09-24 22:59:31 +0530

    deleted unworked shop

--- ShopApp/src/utility/Colors/myColors.js
+++
@@ -1,6 +0,0 @@
-export const myColor = {
-  white: '#FFFFFF',
-  black: '#000000',
-  darkGray: '#1c1c1c',
-  lightGray: '#f1f1f1',
-};
--- ShopApp/store/index.js
+++
@@ -1,3 +0,0 @@
-const store = require('./store');
-const fetchData = require('./productSlice').fetchData;
-console.log()(store.getState());
